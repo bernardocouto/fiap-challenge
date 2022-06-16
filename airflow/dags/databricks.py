05bc5f506105bed3eea7ef5c6888a555b9cd35ef
Bernardo Couto <bernardocouto@icloud.com>
2022-06-16 14:42:10 -0300

    Estrutura inicial da DAG do Databricks

---
+++ airflow/dags/databricks.py
@@ -0,0 +1,34 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksRunNowOperator
+from datetime import datetime, timedelta
+
+import os
+
+default_args = {
+    'depends_on_past': False,
+    'owner': 'fiap',
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5)
+}
+
+with DAG('databricks',
+    catchup=False,
+    default_args=default_args,
+    schedule_interval='@daily',
+    start_date=datetime(2022, 1, 1)
+) as dag:
+
+    heatl_bronze = DatabricksRunNowOperator(
+        databricks_conn_id='databricks_default',
+        job_id=os.environ.get('HEALTH_BRONZE_JOB'),
+        task_id='health_bronze'
+    )
+
+    sales_bronze = DatabricksRunNowOperator(
+        databricks_conn_id='databricks_default',
+        job_id=os.environ.get('SALES_BRONZE_JOB'),
+        task_id='sales_bronze'
+    )
+
+    heatl_bronze
+    sales_bronze
